9ca2201a72b9d262542657fa4949e9c1713ec83d
Aditya200410 <adityakumar098k@gmail.com>
2024-11-15 19:00:50 +0530

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -44,75 +44,73 @@ export default function Login() {
   return (
 
     <div className="topmar">
-    <Stack sx={{ width: '100%', display: 'flex', justifyContent: "center", alignItems: 'center', background: "#021E30", height:"fit-content"}}>
-      <Stack sx={{ display: "flex", flexDirection: "row", width: { xs: "100%", sm: "90%", md: '80%' }, height: { xs: 'fit-content', md: '90dvh' }, boxShadow: "1px 1px 10px 8px #00000048", borderRadius: '8px', border: { xs: "2px solid white", sm: "none" } }}>
-        <Stack sx={{ display: { xs: 'none', md: 'flex' }, justifyContent: 'center', alignItems: 'center', flex: '1', background: `url(https://newdemo.rreda.in/wp-content/uploads/2023/04/cropped-Untitled-design-1.png)`, backgroundPosition: 'center', backgroundRepeat: "no-repeat", backgroundSize: 'cover', borderTopLeftRadius: "8px", borderBottomLeftRadius: "8px" }}>
-        </Stack>
-        <Stack sx={{ display: 'flex', justifyContent: "center", alignItems: "center", flex: "1", height: 'inherit', borderTopRightRadius: '8px', borderBottomRightRadius: "8px", position: 'relative' }}>
-          <Stack sx={{ background: `url(./iic_logo.png)`, height: '100%', display: 'flex', backgroundPosition: "center", backgroundRepeat: 'no-repeat', backgroundSize: 'contain', zIndex: '95', width: '100%', position: "absolute", opacity: '0.1' , height:"fit-content" }}>
-          </Stack>
-          <form onSubmit={handleSubmit} style={{ width: '100%', padding: '1.5rem', height: 'inherit', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '24px', zIndex: '91' }}>
-            <AccountCircleIcon sx={{ fontSize: "6.25rem", color: 'whitesmoke' }} />
-            <Typography variant="h3" sx={{ color: 'white', fontWeight: 'bolder', fontFamily: 'Poppins, sans-serif', borderBottom: '4px solid white', paddingBottom: '0.25rem', borderRadius: '2px' }}>Sign In</Typography>
-            <Stack sx={{ width: '100%' }}>
-              <TextField
-                required   label="Email Address" name='email' type='email' placeholder='Enter email' onChange={(e) => setEmail(e.target.value)} InputLabelProps={{ sx: { color: "whitesmoke", fontSize: { xs: "1.5rem", md: "16px" } } }} sx={{
-                  '& .MuiInputBase-input': {
-                    color: 'whitesmoke',
-                    fontSize: { xs: "1.5rem", md: "16px" }
-                  },
-                  '& .MuiInput-underline:before': {
-                    borderBottomColor: 'white',
-                  },
-                  '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
-                    borderBottomColor: '#00CED1',
-                  },
-                  '& .MuiInput-underline:after': {
-                    borderBottomColor: 'whitesmoke',
-                  },
-                }} variant="standard" />
-            </Stack>
-
-            <Stack sx={{ width: '100%' }}>
-              <TextField
-                required fullWidth id="outlined-required" label="Password" name='password' type='password' placeholder='Enter password' onChange={(e) => setPassword(e.target.value)} InputLabelProps={{ sx: { color: "whitesmoke", fontSize: { xs: "1.5rem", md: "16px" } } }} sx={{
-                  '& .MuiInputBase-input': {
-                    color: 'whitesmoke',
-                    fontSize: { xs: "1.5rem", md: "16px" }
-                  },
-                  '& .MuiInput-underline:before': {
-                    borderBottomColor: 'white',
-                  },
-                  '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
-                    borderBottomColor: '#00CED1',
-                  },
-                  '& .MuiInput-underline:after': {
-                    borderBottomColor: 'whitesmoke',
-                  },
-                }} variant="standard" />
-            </Stack>
-
-
-            <Stack sx={{ width: '100%' }}>
-              <Button variant="contained" sx={{
-                background: "#00CED1", color: '#021E30', padding: '8px 0px', borderRadius: '.75rem', '&:hover': {
-                  transform: "scale(1.001)",
-                  background: 'white',
-                  color: "#00ced1",
-                  fontWeight: "500",
-                  border: ".125rem solid #00ced1"
-                }
-              }} type="submit" >
-                Sign in
-              </Button>
-            </Stack>
-            <Typography sx={{ color: 'whitesmoke', textAlign: 'center' }}>
-              Don't have an account ? <NavLink  to="/checkout" style={{ color: '#00CED1' }}>Sign Up</NavLink>
-            </Typography>
-          </form>
-        </Stack>
-      </Stack>
+   <Stack sx={{ width: '100%', display: 'flex', justifyContent: "center", alignItems: 'center', background: "white", height: "fit-content" }}>
+  <Stack sx={{ display: "flex", flexDirection: "row", width: { xs: "100%", sm: "90%", md: '80%' }, height: { xs: 'fit-content', md: '90dvh' }, borderRadius: '8px', border: { xs: "2px solid white", sm: "none" } }}>
+    <Stack sx={{ display: { xs: 'none', md: 'flex' }, justifyContent: 'center', alignItems: 'center', flex: '1', background: `url(https://newdemo.rreda.in/wp-content/uploads/2023/04/cropped-Untitled-design-1.png)`, backgroundPosition: 'center', backgroundRepeat: "no-repeat", backgroundSize: 'cover', borderTopLeftRadius: "8px", borderBottomLeftRadius: "8px" }}>
     </Stack>
+    <Stack sx={{ display: 'flex', justifyContent: "center", alignItems: "center", flex: "1", height: 'inherit', borderTopRightRadius: '8px', borderBottomRightRadius: "8px", position: 'relative' }}>
+      <Stack sx={{ background: `url(./iic_logo.png)`, height: '100%', display: 'flex', backgroundPosition: "center", backgroundRepeat: 'no-repeat', backgroundSize: 'contain', zIndex: '0', width: '100%', position: "absolute", opacity: '0.1', height: "fit-content" }}>
+      </Stack>
+      <form onSubmit={handleSubmit} style={{ width: '100%', padding: '1.5rem', height: 'inherit', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '24px', zIndex: '0' }}>
+        <AccountCircleIcon sx={{ fontSize: "6.25rem", color: 'black' }} />
+        <Typography variant="h3" sx={{ color: 'black', fontWeight: 'bolder', fontFamily: 'Poppins, sans-serif', borderBottom: '4px solid black', paddingBottom: '0.25rem', borderRadius: '2px' }}>Sign In</Typography>
+        <Stack sx={{ width: '100%' }}>
+          <TextField
+            required label="Email Address" name='email' type='email' placeholder='Enter email' onChange={(e) => setEmail(e.target.value)} InputLabelProps={{ sx: { color: "black", fontSize: { xs: "1.5rem", md: "16px" } } }} sx={{
+              '& .MuiInputBase-input': {
+                color: 'black',
+                fontSize: { xs: "1.5rem", md: "16px" }
+              },
+              '& .MuiInput-underline:before': {
+                borderBottomColor: 'black',
+              },
+              '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
+                borderBottomColor: '#00CED1',
+              },
+              '& .MuiInput-underline:after': {
+                borderBottomColor: 'black',
+              },
+            }} variant="standard" />
+        </Stack>
+
+        <Stack sx={{ width: '100%' }}>
+          <TextField
+            required fullWidth id="outlined-required" label="Password" name='password' type='password' placeholder='Enter password' onChange={(e) => setPassword(e.target.value)} InputLabelProps={{ sx: { color: "black", fontSize: { xs: "1.5rem", md: "16px" } } }} sx={{
+              '& .MuiInputBase-input': {
+                color: 'black',
+                fontSize: { xs: "1.5rem", md: "16px" }
+              },
+              '& .MuiInput-underline:before': {
+                borderBottomColor: 'black',
+              },
+              '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
+                borderBottomColor: '#00CED1',
+              },
+              '& .MuiInput-underline:after': {
+                borderBottomColor: 'black',
+              },
+            }} variant="standard" />
+        </Stack>
+
+        <Stack sx={{ width: '100%' }}>
+          <Button variant="contained" sx={{
+            background: "#00CED1", color: 'black', padding: '8px 0px', borderRadius: '.75rem', '&:hover': {
+              transform: "scale(1.001)",
+              background: 'white',
+              color: "#00ced1",
+              fontWeight: "500",
+              border: ".125rem solid #00ced1"
+            }
+          }} type="submit" >
+            Sign in
+          </Button>
+        </Stack>
+        
+      </form>
+    </Stack>
+  </Stack>
+</Stack>
+
     </div>
   );
 }
--- src/components/Resorts.jsx
+++ src/components/Resorts.jsx
@@ -1,8 +1,56 @@
 import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+
+import { Stack, Button, Typography, TextField, } from '@mui/material';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 
 const Resorts = () => {
+  const [fname, setFname] = useState("");
+  const [pnum, setPnum] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [adult, setadult] = useState("");
+  const [child, setchild] = useState("");
+  const [date, setDate] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+
+      e.preventDefault();
+
+      console.log(fname, pnum,  email, password,adult,child,date);
+      fetch("https://iic-backend-r3jg.onrender.com/register", {
+          method: "POST",
+          crossDomain: true,
+          headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+              "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({
+              fname,
+              email,
+              pnum,
+              adult,
+              child,
+              date,
+              password,
+
+          }),
+      })
+          .then((res) => res.json())
+          .then((data) => {
+              console.log(data, "userRegister");
+              if (data.status === "ok") {
+                  alert("Registration Successful");
+                  setTimeout(() => {
+                    navigate('/sign-in');
+                  }, 500);
+              } else {
+                  alert("Something went wrong");
+              }
+          });
+  };
 
   const [adultCount, setAdultCount] = useState(1);
   const [childCount, setChildCount] = useState(0);
@@ -306,8 +354,130 @@ const tripinfo = [
         <label>Pickup & Drop Service</label>
       </div>
 
-     <NavLink to='/checkout'> <button className="checkout-button">PROCEED TO CHECKOUT</button></NavLink>
+
+
+      <div >
+      <Stack sx={{ width: '100%', display: 'flex', justifyContent: "center", alignItems: 'center', height: "100dvh", background: "white", height: "fit-content",marginTop:"20px",marginBottom:"50px" }}>
+    <Stack sx={{ display: "flex", flexDirection: "row", width: { xs: '100%', sm: '85%', md: '80%' }, height: { xs: "fit-content", sm: "90dvh", md: '90dvh' }, borderRadius: '8px', border: { xs: "2px solid white", sm: "none" },zIndex:"0" }}>
+        <form onSubmit={handleSubmit} style={{ width: '100%', padding: '1.5rem', height: 'inherit', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', gap: '8px', zIndex: '200' }}>
+            <Typography variant="h3" sx={{ color: 'black', fontWeight: 'bolder', fontFamily: 'Poppins, sans-serif', borderBottom: '4px solid black', paddingBottom: '0.25rem', borderRadius: '2px' }}>Sign Up</Typography>
+            <Stack sx={{ width: '100%' }}>
+                <TextField
+                    required
+                    label="Full Name"
+                    name="full name"
+                    type="text"
+                    
+                    placeholder="Enter full name"
+                    onChange={(e) => setFname(e.target.value)}
+                    InputLabelProps={{ sx: { color: "black", fontSize: { xs: "1.5rem", md: "16px" } } }}
+                    sx={{
+                        '& .MuiInputBase-input': { color: 'black', fontSize: { xs: "1.5rem", md: "16px" } },
+                        '& .MuiInput-underline:before': { borderBottomColor: 'black' },
+                        '& .MuiInput-underline:hover:not(.Mui-disabled):before': { borderBottomColor: '#00CED1' },
+                    }}
+                    variant="standard"
+                />
+            </Stack>
+
+            <Stack sx={{ width: '100%' }}>
+                <TextField
+                    required
+                    fullWidth
+                    id="outlined-required"
+                    label="Email Address"
+                    name="email"
+                    type="email"
+                    placeholder="Enter email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    InputLabelProps={{ sx: { color: "black", fontSize: { xs: "1.5rem", md: "16px" } } }}
+                    sx={{
+                        '& .MuiInputBase-input': { color: 'black', fontSize: { xs: "1.5rem", md: "16px" } },
+                        '& .MuiInput-underline:before': { borderBottomColor: 'black' },
+                        '& .MuiInput-underline:hover:not(.Mui-disabled):before': { borderBottomColor: '#00CED1' },
+                        '& .MuiInput-underline:after': { borderBottomColor: 'black' },
+                    }}
+                    variant="standard"
+                />
+            </Stack>
+
+            <Stack gap={2} sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+                <TextField
+                    required
+                    fullWidth
+                    id="outlined-required"
+                    label="Phone Number"
+                    name="Phno"
+                    type="number"
+                    placeholder="Enter Phone number"
+                    onChange={(e) => setPnum(e.target.value)}
+                    InputLabelProps={{ sx: { color: "black", fontSize: { xs: "1.5rem", md: "16px" } } }}
+                    sx={{
+                        '& .MuiInputBase-input': { color: 'black', fontSize: { xs: "1.5rem", md: "16px" } },
+                        '& .MuiInput-underline:before': { borderBottomColor: 'black' },
+                        '& .MuiInput-underline:hover:not(.Mui-disabled):before': { borderBottomColor: '#00CED1' },
+                        '& .MuiInput-underline:after': { borderBottomColor: 'black' },
+                    }}
+                    variant="standard"
+                />
+            </Stack>
+
+            <Stack sx={{ width: '100%' }}>
+                <TextField
+                    required
+                    fullWidth
+                    id="outlined-required"
+                    label="Password"
+                    name="password"
+                    type="password"
+                    placeholder="Enter password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    InputLabelProps={{ sx: { color: "black", fontSize: { xs: "1.5rem", md: "16px" } } }}
+                    sx={{
+                        '& .MuiInputBase-input': { color: 'black', fontSize: { xs: "1.5rem", md: "16px" } },
+                        '& .MuiInput-underline:before': { borderBottomColor: 'black' },
+                        '& .MuiInput-underline:hover:not(.Mui-disabled):before': { borderBottomColor: '#00CED1' },
+                        '& .MuiInput-underline:after': { borderBottomColor: 'black' },
+                    }}
+                    variant="standard"
+                />
+            </Stack>
+            <Stack sx={{ width: '100%' }}>
+                <Button
+                    variant="contained"
+                    sx={{
+                        background: "#00CED1",
+                        color: 'black',
+                        padding: '8px 0px',
+                        borderRadius: '.75rem',
+                        fontSize: { xs: "1.15rem", md: "16px" },
+                        '&:hover': {
+                            transform: "scale(1.001)",
+                            background: 'white',
+                            color: "#00ced1",
+                            fontWeight: "500",
+                            border: ".125rem solid #00ced1",
+                        },
+                    }}
+                    type="submit"
+                >
+                    Sign Up and Book
+                </Button>
+                
+            </Stack>
+            <Typography sx={{ color: 'black', textAlign: 'center' }}>
+                Returning Customer? <NavLink to="/sign-in" style={{ color: '#00CED1' }}>Sign in</NavLink>
+            </Typography>
+        </form>
+    </Stack>
+</Stack>
+
+      
+        </div>
+     
     </div>
+
+
     </>
   );
 };
